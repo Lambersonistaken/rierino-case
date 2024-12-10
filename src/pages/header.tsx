6286eaa841ad1149d2573acaab768971f8020d03
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2024-12-10 17:52:21 +0300

    header section done

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -1,12 +1,16 @@
-
-
+import Logo from "../../public/logo.svg";
+import Image from "next/image";
+import {Button} from "@/components/ui/button"
 
 export default function Header() {
     return (
-      <header>
-        <div className="container flex justify-between items-center">
-            
+      <header className="flex flex-row justify-between items-center p-4 border-b border-gray-200">
+        <div className="flex items-center justify-center gap-6">
+        <Image src={Logo} width={30} className="ml-6" alt="logo"  />
+        <h2 className="font-semibold">Home</h2>
         </div>
+        <Button className="mr-6">Export</Button>
+
       </header>
     );
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,22 @@
-// import Image from "next/image";
-// import localFont from "next/font/local";
+import localFont from "next/font/local";
 
-// const geistSans = localFont({
-//   src: "./fonts/GeistVF.woff",
-//   variable: "--font-geist-sans",
-//   weight: "100 900",
-// });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+import Header from "./header";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className={`${geistSans.variable} ${geistMono.variable} font-[family-name:var(--font-geist-sans)]`}>
+        <Header />
     </div>
   );
 }
